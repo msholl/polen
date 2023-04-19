22d201a0c66633f4d2075ae916bacd0cca94c065
msholl <matheus@conetca-soft.com.br>
2023-04-19 17:39:51 -0300

    Criando feature de entregas

---
+++ database/migrations/2023_04_19_202416_create_entregas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entregas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 128);
+            $table->string('endereco', 128)->nullable();
+            $table->integer('numero')->nullable();
+            $table->string('bairro', 128);
+            $table->integer('ordem')->nullable();
+            $table->boolean('entregue')->default(false);
+            $table->date('data_entrega')->nullable();
+            $table->string('entregador')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entregas');
+    }
+};
